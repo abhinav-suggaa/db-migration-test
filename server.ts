62dbe1f232ec886b26c85fb1ea2d3262822312f9
abhinav-suggaa <abhinav@suggaa.in>
2023-06-09 15:35:06 +0530

    configured api endpoints in server.ts

--- server.ts
+++ server.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import companyRouter from "./companies";
+import jobRouter from "./jobListings";
+
 const app = express();
 app.use(express.json());
+app.use("api/companies", companyRouter);
+app.use("api/jobs", jobRouter);
 
 app.listen(1337, () => console.log("server is  running on port 1337"));
